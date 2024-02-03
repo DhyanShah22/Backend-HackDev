ce3afe97e4cc24acd5e8f5d05a6cf0c4ddb6ce7a
DhyanShah22 <dhyanshah229@gmail.com>
2024-02-03 18:38:06 +0530

    Added middelware to calculate response time

--- Sellers/server.js
+++ Sellers/server.js
@@ -1,26 +1,42 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const  SellerRoutes = require('./Routes/sellAuthRoutes')
-const sellpath = require('./Routes/sellRoutes')
-const app = express()
+const SellerRoutes = require('./Routes/sellAuthRoutes');
+const sellpath = require('./Routes/sellRoutes');
+const app = express();
 
-app.use(express.json())
+// Middleware to measure response time manually
+app.use((req, res, next) => {
+    const start = performance.now();
+  
+    // Your routes and other middleware go here
+  
+    const end = performance.now();
+    const responseTime = end - start;
+  
+    console.log(`Response time: ${responseTime}ms`);
+  
+    next();
+  });
+  
 
-app.use((req,res,next) => {
-    console.log(req.path, req.method)
-    next()
-})
-app.use('/api/Seller', SellerRoutes)
-app.use('/api/sellProp', sellpath)
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-mongoose.connect(process.env.MONG_URI)
-    .then(() => {
-        app.listen((process.env.PORT), () => {
-            console.log('Connected to DB and listening to port', process.env.PORT)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+app.use(express.json());
+app.use('/api/Seller', SellerRoutes);
+app.use('/api/sellProp', sellpath);
+
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to DB and listening to port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
